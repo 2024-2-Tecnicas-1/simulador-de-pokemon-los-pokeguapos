54cd3c11dbfe2c81be5d490ed06b078fbdf1b497
LuFer15 <luisafernandap70@gmail.com>
2024-11-02 11:56:37 -0500

    Se agrega la clase del Pokémon Machop

---
+++ src/main/java/simulador/logicaNegocio/Pokemones/Machop.java
@@ -0,0 +1,15 @@
+package simulador.logicaNegocio.Pokemones;
+
+import simulador.logicaNegocio.pokemon.Estados;
+import simulador.logicaNegocio.pokemon.Pokemon;
+import simulador.logicaNegocio.pokemon.TipoPokemon;
+
+public class Machop extends Pokemon {
+
+    public Machop (String nombre, int salud, int puntosDeAtaque, TipoPokemon tipo, Estados estado){
+
+        super(nombre, salud, puntosDeAtaque, tipo, estado);
+    }
+
+
+}
