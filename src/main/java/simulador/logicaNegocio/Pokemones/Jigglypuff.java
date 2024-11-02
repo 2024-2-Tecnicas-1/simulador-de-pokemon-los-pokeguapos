12116f5c65adcb9f2f9562194037cd5e61cc683b
LuFer15 <luisafernandap70@gmail.com>
2024-11-02 12:01:15 -0500

    Se agrega la clase del Pokémon Jigglypuff

---
+++ src/main/java/simulador/logicaNegocio/Pokemones/Jigglypuff.java
@@ -0,0 +1,14 @@
+package simulador.logicaNegocio.Pokemones;
+
+import simulador.logicaNegocio.pokemon.Estados;
+import simulador.logicaNegocio.pokemon.Pokemon;
+import simulador.logicaNegocio.pokemon.TipoPokemon;
+
+public class Jigglypuff extends Pokemon {
+
+    public Jigglypuff(String nombre, int salud, int puntosDeAtaque, TipoPokemon tipo, Estados estado){
+
+        super(nombre, salud, puntosDeAtaque, tipo, estado);
+    }
+
+}
