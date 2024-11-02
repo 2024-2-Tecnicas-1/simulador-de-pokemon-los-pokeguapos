e4ce188c1a8ff3d37baafb1a696bc04f8b1d80ff
LuFer15 <luisafernandap70@gmail.com>
2024-11-02 11:27:55 -0500

    Se agrega la clase del Pokémon Geodude

---
+++ src/main/java/simulador/logicaNegocio/Pokemones/Geodude.java
@@ -0,0 +1,15 @@
+
+
+import simulador.logicaNegocio.pokemon.Estados;
+import simulador.logicaNegocio.pokemon.Pokemon;
+import simulador.logicaNegocio.pokemon.TipoPokemon;
+
+public class Geodude extends Pokemon {
+
+    public Geodude(String nombre, int salud, int puntosDeAtaque, TipoPokemon tipo, Estados estado){
+
+            super(nombre, salud, puntosDeAtaque, tipo, estado);
+
+    }
+        
+}
