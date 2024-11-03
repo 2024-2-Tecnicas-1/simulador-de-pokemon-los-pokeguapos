a9ed3e90b51351949ecaec66c65a1591b772ddc5
damoelrrr <danielfeliperodriguezangulo@gmail.com>
2024-11-03 09:50:07 -0500

    Se agraga la clase del pokemon Squirtle

--- src/main/java/simulador/logicaNegocio/Pokemones/Charmander.java
+++ src/main/java/simulador/logicaNegocio/Pokemones/Charmander.java
@@ -1,5 +1,13 @@
 package simulador.logicaNegocio.Pokemones;
 
-public class Charmander {
+import simulador.logicaNegocio.pokemon.Estados;
+import simulador.logicaNegocio.pokemon.Pokemon;
+import simulador.logicaNegocio.pokemon.TipoPokemon;
+
+public class Charmander extends Pokemon {
+    
+    public Charmander(String nombre, int salud, int puntosDeAtaque, TipoPokemon tipo, Estados estado) {
+        super(nombre, salud, puntosDeAtaque, tipo, estado);
+    }
     
 }
