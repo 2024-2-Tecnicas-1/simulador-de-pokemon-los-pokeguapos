c18f691a8473df7fe794af1b3840b2854cbaf23b
sebasanguloc <sebasanguloc@gmail.com>
2024-11-20 22:45:05 -0500

    Eliminacion de algunos comentarios

--- src/main/java/simulador/Principal.java
+++ src/main/java/simulador/Principal.java
@@ -4,12 +4,7 @@ package simulador;
 public class Principal {
     public static void main(String[] args) {
         Controller start = new Controller();
-        /*
-        Pokemon charmander = new Charmander("Charmander", 39, 52, TipoPokemon.FUEGO, Estados.NORMAL);
-        Pokemon bulbasaur = new Bulbasaur("Bulbasaur", 45, 49, TipoPokemon.PLANTA, Estados.NORMAL);
-        BatallaForm batalla = new BatallaForm(charmander,bulbasaur);
-        batalla.setVisible(true);
-        */
+
         start.run();
     }
 }
